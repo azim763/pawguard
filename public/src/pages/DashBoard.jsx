02b4fc65307ce922430c8c154fc99bbbbfc93ebb
azim763 <azim763@yahoo.com>
2023-09-29 17:06:39 -0700

    b1

---
+++ public/src/pages/DashBoard.jsx
@@ -0,0 +1,77 @@
+import React, { useEffect, useState, useRef } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { io } from "socket.io-client";
+import styled from "styled-components";
+import { allUsersRoute, host } from "../utils/APIRoutes";
+import Logout from "../components/Logout";
+
+ import Background from "../assets/background2.gif";
+
+export default function Chat() {
+  const navigate = useNavigate();
+  const socket = useRef();
+  const [currentUser, setCurrentUser] = useState(undefined);
+  useEffect(async () => {
+    if (!localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY)) {
+      navigate("/login");
+    } else {
+      setCurrentUser(
+        await JSON.parse(
+          localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY)
+        )
+      );
+    }
+  }, []);
+  useEffect(() => {
+    if (currentUser) {
+      socket.current = io(host);
+      socket.current.emit("add-user", currentUser._id);
+    }
+  }, [currentUser]);
+
+  useEffect(async () => {
+    if (currentUser) {
+      if (currentUser.isAvatarImageSet) {
+        const data = await axios.get(`${allUsersRoute}/${currentUser._id}`);
+
+      } else {
+        navigate("/setAvatar");
+      }
+    }
+  }, [currentUser]);
+
+  return (
+    <>
+      <Container>
+        <div className="container">
+        <Logout />
+        </div>
+      </Container>
+    </>
+  );
+}
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 1rem;
+  align-items: center;
+  background-color: #dedfdc;
+  .container {
+    height: 85vh;
+    width: 85vw;
+  /*  background-image: url("${Background}"); */
+    background-repeat: repeat;
+    
+    background-color:#efeae2;
+    /*   display: grid;  */
+    grid-template-columns: 25% 75%;
+    @media screen and (min-width: 720px) and (max-width: 1080px) {
+      grid-template-columns: 35% 65%;
+    }
+  }
+`;
