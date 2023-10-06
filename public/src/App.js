2a09dc193eb66896214669fcd84f2984d932aaa8
Yuki Cheng <hcheng40@mylangara.ca>
2023-10-06 13:31:59 -0700

    added typography and global variables

--- public/src/App.js
+++ public/src/App.js
@@ -4,6 +4,7 @@ import SetAvatar from "./components/SetAvatar";
 import DashBoard from "./pages/DashBoard";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import "./utils/variable.css"
 export default function App() {
   return (
     <BrowserRouter>
