eb6a67ab0755235388bb49d70fef1887055acbe3
thalhauzair1 <135381947+thalhauzair1@users.noreply.github.com>
2023-10-09 15:54:45 -0700

    added list insurances page

---
+++ public/src/pages/ListInsurances/ListInsurances.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Header from '../../components/Header/header'
+import Typography from '../../components/Typography/Typography'
+
+const ListInsurances = () => {
+  return (
+    <div>
+        <Header></Header>
+        <Typography variant="sub-h2-poppins-medium">
+            Plans Recommend for you
+
+        </Typography>
+        
+      
+    </div>
+  )
+}
+
+export default ListInsurances
