949cc08211189211d232c3caf79a0de14468c2c1
Harnoor Kaur <harnoor1009@gmail.com>
2023-10-09 08:22:04 -0700

    insCard and textArea components added

---
+++ public/src/components/InsuranceCard/InsuranceCard.jsx
@@ -0,0 +1,26 @@
+import React  from "react";
+import './insuranceCard.module.css';
+
+function InsuranceCard({title,text,subtitle,body}){
+    return(
+        <div className="card-container">
+            <div className="card-title">
+                <h3>{title}</h3>
+                <h5>{text}</h5>
+            </div>
+            <div className="card-body">
+                <h4>{subtitle}</h4>
+                <p>{body}</p>   
+            </div> 
+        </div>
+    )
+}
+
+// to use the card component :-
+
+// <InsuranceCard
+//     title=""
+//     text=""
+//     subtitle=""
+//     body=""
+// />
