e620273c6027e64b35f833571bda9c9b93b874b4
Yuki Cheng <hcheng40@mylangara.ca>
2023-10-16 14:53:44 -0700

    [For Review] IndividualClinicPage

---
+++ public/src/pages/IndividualClinic/IndividualClinic.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../../components/Header/header";
+import Typography from "../../components/Typography/Typography";
+import PlanDetailCard from "../../components/PlanDetailCard/PlanDetailCard";
+import styles from "./listInsurances.module.css";
+
+const IndividualClinic = () => {
+  return (
+    <div>
+      <Header></Header>
+    </div>
+  );
+};
+
+export default IndividualClinic;
