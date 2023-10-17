e76310d9706fde783efa15bde8860deba2828851
Yuki Cheng <hcheng40@mylangara.ca>
2023-10-16 19:26:30 -0700

    [For Review] ClinicSpecialiesCard

--- public/src/pages/IndividualClinic/IndividualClinic.jsx
+++ public/src/pages/IndividualClinic/IndividualClinic.jsx
@@ -2,16 +2,28 @@ import React from "react";
 import Header from "../../components/Header/header";
 import Typography from "../../components/Typography/Typography";
 import ClinicContactCard from "../../components/ClinicContactCard/ClinicContactCard";
+import ClinicSpecialtiesCard from "../../components/ClinicSpecialtiesCard/ClinicSpecialtiesCard";
 import styles from "./individualClinic.module.css";
 
 const IndividualClinic = () => {
+
+  const specialties = ["Dentistry", "Allergy"];
+
   return (
     <div>
       <div>
         <Header></Header>
       </div>
       <div>
-        <ClinicContactCard clinicTel="+1 234 313-3213" clinicUrl="www.clinic.com" />
+        <ClinicContactCard
+          clinicTel="+1 234 313-3213"
+          clinicUrl="www.clinic.com"
+        />
+      </div>
+      <div>
+        <ClinicSpecialtiesCard 
+        key="1"
+        specialties={specialties} />
       </div>
     </div>
   );
