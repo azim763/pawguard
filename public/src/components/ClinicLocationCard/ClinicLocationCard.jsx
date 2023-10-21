c8b4fee3caed213325cda21db8dd656fbce21bc8
Yuki Cheng <hcheng40@mylangara.ca>
2023-10-20 18:03:13 -0700

    [For Review] Clinic Location Card with Map

--- public/src/components/ClinicLocationCard/ClinicLocationCard.jsx
+++ public/src/components/ClinicLocationCard/ClinicLocationCard.jsx
@@ -1,11 +1,46 @@
-import React from "react";
+import React, { useEffect } from "react";
+import ReactDOM from "react-dom";
+import tt from "@tomtom-international/web-sdk-maps";
 import styles from "./clinicLocationCard.module.css";
 import Typography from "../Typography/Typography";
 import AlertSVG from "../SVG/AlertSVG";
 import ClockSVG from "../SVG/ClockSVG";
 import LocationSVG from "../SVG/LocationSVG";
 
-const ClinicLocationCard = ({ address, hours, urgentCare }) => {
+const ClinicLocationCard = ({
+  address,
+  hours,
+  urgentCare,
+  latitude,
+  longitude,
+  markerlat,
+  markerlong
+}) => {
+  useEffect(() => {
+    const map = tt.map({
+      key: "2TseK96GRlPI1NdG2lpm0nMvDK4fwDWv",
+      container: "map-container",
+      center: [longitude, latitude], 
+      zoom: 10,
+    });
+
+    // const customMarkerElement = document.createElement("div");
+    // ReactDOM.render(<LocationSVG width="23" height="34" />, customMarkerElement);
+
+    // const marker = new tt.Marker({
+    //   element: customMarkerElement,
+    //   anchor: [0, 0], 
+    // })
+    //   .setLngLat([markerlong, markerlat])
+    //   .addTo(map);
+
+    // console.log(markerlong);
+
+    return () => {
+      map.remove();
+    };
+  }, [latitude, longitude, markerlat, markerlong]); 
+
   return (
     <div className={`${styles["location-card-container"]}`}>
       <div className={`${styles["heading-container"]}`}>
@@ -22,6 +57,9 @@ const ClinicLocationCard = ({ address, hours, urgentCare }) => {
       <div className={`${styles["alert-container"]}`}>
         <AlertSVG width="30" height="30" />
         {urgentCare}
+      </div>
+      <div id="map-container" className={`${styles["map-container"]}`}>
+        {/* This is where the map will be rendered */}
       </div>
     </div>
   );
--- public/src/pages/IndividualClinic/IndividualClinic.jsx
+++ public/src/pages/IndividualClinic/IndividualClinic.jsx
@@ -5,27 +5,45 @@ import ClinicContactCard from "../../components/ClinicContactCard/ClinicContactC
 import ClinicSpecialtiesCard from "../../components/ClinicSpecialtiesCard/ClinicSpecialtiesCard";
 import styles from "./individualClinic.module.css";
 import ClinicLocationCard from "../../components/ClinicLocationCard/ClinicLocationCard";
+import Aws from "../../components/ClinicOperationsHours/OperHrsCard";
 
 const IndividualClinic = () => {
   const specialties = ["Dentistry", "Allergy"];
+  const hoursOfOperation = [
+    { day: "Monday", timeSlots: ["9:00 AM - 5:00 PM", "6:00 PM - 9:00 PM"] },
+    { day: "Tuesday", timeSlots: ["10:00 AM - 4:00 PM"] },
+  ];
 
   return (
     <div>
       <div>
         <Header></Header>
       </div>
-      <div>
-        <ClinicContactCard
-          clinicTel="+1 234 313-3213"
-          clinicUrl="www.clinic.com"
-        />
-      </div>
-      <div>
-        <ClinicSpecialtiesCard key="1" specialties={specialties} />
-      </div>
+      <div className={styles.IndividualClinicContainer}>
+        <div>
+          <ClinicLocationCard
+            address="1541 Kingsway, Vancouver, BC V5N 2R8"
+            hours="Open 24 Hours"
+            urgentCare="urgent care clinic"
+            latitude={49.246292}
+            longitude={-123.116226}
+            // markerlong={-123.116226}
+            // markerlat={49.246292}
+          />
+        </div>
+        <div className={styles.ClinicContact}>
+          <ClinicContactCard
+            clinicTel="+1 234 313-3213"
+            clinicUrl="www.clinic.com"
+          />
+        </div>
+        <div className={styles.ClinicSpeciality}>
+          <ClinicSpecialtiesCard key="1" specialties={specialties} />
+        </div>
 
-      <div>
-        <ClinicLocationCard address="1541 Kingsway, Vancouver, BC V5N 2R8" hours="Open 24 Hours" urgentCare="urgent care clinic" />
+        <div className={styles.OpenHrs}>
+          <Aws hoursOfOperation={hoursOfOperation} />
+        </div>
       </div>
     </div>
   );
