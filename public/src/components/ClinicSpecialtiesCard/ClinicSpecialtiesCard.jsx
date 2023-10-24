2feae1cafe0db3f69bf4ed498c6bfdb806562cb1
Yuki Cheng <hcheng40@mylangara.ca>
2023-10-24 14:27:28 -0700

    [For Review] Individual Clinic Page BE, components bug fixed

--- public/src/components/ClinicSpecialtiesCard/ClinicSpecialtiesCard.jsx
+++ public/src/components/ClinicSpecialtiesCard/ClinicSpecialtiesCard.jsx
@@ -1,18 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./clinicSpecialtiesCard.module.css";
 import Typography from "../Typography/Typography";
 
-const ClinicSpecialtiesCard = ({ specialties }) => {
+const ClinicSpecialtiesCard = ({ specialtiesCardString }) => {
+  const [specialtiesCardArray, setSpecialtiesCardArray] = useState([]);
+
+  useEffect(() => {
+    const specialtiesCardArray = specialtiesCardString.split(",");
+    setSpecialtiesCardArray(specialtiesCardArray);
+  }, [specialtiesCardString]);
+
   return (
     <div className={`${styles["specialty-card-container"]}`}>
       <div className={`${styles["heading-container"]}`}>
         <Typography variant="sub-h1-poppins-semibold">Specialties</Typography>
       </div>
       <div className={`${styles["specialties-container"]}`}>
-        <ul style={{paddingLeft: "5%"}}>
-          {specialties.map((specialty, index) => (
-            <li key={index}>{specialty}</li>
+        <ul style={{ paddingLeft: "5%" }}>
+        <Typography variant="sub-h2-poppins-medium">
+          {specialtiesCardArray.map((item, index) => (
+            <li key={index}>{item}</li>
           ))}
+          </Typography>
         </ul>
       </div>
     </div>
