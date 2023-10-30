36bf7a165f565b94db2cbe95149b777e4596ab83
thalhauzair1 <135381947+thalhauzair1@users.noreply.github.com>
2023-10-30 16:12:15 -0700

    made mobileview for header component

--- public/src/pages/Home/Home.jsx
+++ public/src/pages/Home/Home.jsx
@@ -2,37 +2,39 @@ import React,{useState,useEffect} from 'react'
 import axios from 'axios'
 import Graph from '../../components/Graph/Graph'
 import TotalPets from '../../components/TotalPets/TotalPets'
+import Header from '../../components/Header/header'
 import {
   searchPetsByUserIDRoute,
 } from "../../utils/APIRoutes.js";
 
 const Home = () => {
-  const [pets, setPets] = useState([]);
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const storedData = localStorage.getItem(
-          process.env.REACT_APP_LOCALHOST_KEY
-        );
-        if (storedData) {
-          const data = JSON.parse(storedData);
-          const response = await axios.get(searchPetsByUserIDRoute, {
-            params: { userID: data._id },
-          });
-          await setPets(response.data);
-          console.log(response.data); // Log the response data here
-        }
-      } catch (error) {
-        // Handle any errors here
-      }
-    };
+  // const [pets, setPets] = useState([]);
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const storedData = localStorage.getItem(
+  //         process.env.REACT_APP_LOCALHOST_KEY
+  //       );
+  //       if (storedData) {
+  //         const data = JSON.parse(storedData);
+  //         const response = await axios.get(searchPetsByUserIDRoute, {
+  //           params: { userID: data._id },
+  //         });
+  //         await setPets(response.data);
+  //         console.log(response.data); // Log the response data here
+  //       }
+  //     } catch (error) {
+  //       // Handle any errors here
+  //     }
+  //   };
     
-    fetchData();
-  }, []);
+  //   fetchData();
+  // }, []);
 
   return (
     <div>
-      {pets && <TotalPets pets={pets}></TotalPets>}
+      {/* {pets && <TotalPets pets={pets}></TotalPets>} */}
+      <Header></Header>
     </div>
   )
 }
