bca5e47b9a26e64bb674770f7998b826dfc1a4e4
azim763 <azim763@yahoo.com>
2023-11-01 01:54:21 -0700

    Login

--- public/src/pages/Login.jsx
+++ public/src/pages/Login.jsx
@@ -6,7 +6,9 @@ import Logo from "../assets/logo.svg";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { loginRoute } from "../utils/APIRoutes";
-import Button from "../components/Button";
+import Button from "../components/Button/Button";
+import Header from "../components/Header/header";
+import loginbackground from "../assets/images/loginback.jpeg";
 
 export default function Login() {
   const navigate = useNavigate();
@@ -64,11 +66,26 @@ export default function Login() {
 
   return (
     <>
+<Header/>
       <FormContainer>
+
+
+      <div class="parent clearfix">
+    <div class="bg-illustration">
+      {/* <img src="https://i.ibb.co/Pcg0Pk1/logo.png" alt="logo"/>
+
+      <div class="burger-btn">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div> */}
+
+    </div>
+<div class="signinup">
         <form action="" onSubmit={(event) => handleSubmit(event)}>
           <div className="brand">
-            <img src={Logo} alt="logo" />
-            <h1>PawGuard!</h1>
+            {/* <img src={Logo} alt="logo" /> */}
+            <h1>Sign In</h1>
           </div>
           <input
             type="text"
@@ -83,12 +100,15 @@ export default function Login() {
             name="password"
             onChange={(e) => handleChange(e)}
           />
-          <button type="submit">Log In</button>
+          {/* <button type="submit">Log In</button> */}
           {/* <Button color={"red"} text={"Add"} onClick={"onAdd"} /> */}
+          <Button variant="yellow" type="submit" label={"Log In"} size="dk-md-s" />
           <span>
             Don't have an account ? <Link to="/register">Create One.</Link>
           </span>
         </form>
+        </div>
+        </div> 
       </FormContainer>
       <ToastContainer />
     </>
@@ -102,8 +122,9 @@ const FormContainer = styled.div`
   flex-direction: column;
   justify-content: center;
   gap: 1rem;
-  align-items: center;
-  background-color: #dedfdc;
+//  align-items: center;
+ // background-color: #dedfdc;
+ 
   .brand {
     display: flex;
     align-items: center;
@@ -122,14 +143,14 @@ const FormContainer = styled.div`
     display: flex;
     flex-direction: column;
     gap: 2rem;
-    background-color: #fff;
-    border-radius: 2rem;
+    // background-color: #fff;
+    // border-radius: 2rem;
     padding: 5rem;
   }
   input {
     background-color: transparent;
     padding: 1rem;
-    border: 0.1rem solid #01a783;
+    border: 0.1rem solid #54656f;
     border-radius: 0.4rem;
     color: #54656f;
     width: 100%;
@@ -139,27 +160,160 @@ const FormContainer = styled.div`
       outline: none;
     }
   }
-  button {
-    background-color: #01a783;
-    color: white;
-    padding: 1rem 2rem;
-    border: none;
-    font-weight: bold;
-    cursor: pointer;
-    border-radius: 0.4rem;
-    font-size: 1rem;
-    text-transform: uppercase;
-    &:hover {
-      background-color: #01a783;
-    }
-  }
+  // button {
+  //   background-color: #01a783;
+  //   color: white;
+  //   padding: 1rem 2rem;
+  //   border: none;
+  //   font-weight: bold;
+  //   cursor: pointer;
+  //   border-radius: 0.4rem;
+  //   font-size: 1rem;
+  //   text-transform: uppercase;
+  //   &:hover {
+  //     background-color: #01a783;
+  //   }
+  // }
   span {
     color: #54656f;
     text-transform: uppercase;
     a {
-      color: #01a783;
+      color: #1F3E5B;
       text-decoration: none;
       font-weight: bold;
+    }
+  }
+
+.signinup
+{
+
+  background-color: var(--off-white);
+
+  min-width: 480px;
+  justify-content: center;
+  display: flex;
+  gap: 1rem;
+  -moz-box-align: center;
+  align-items: center;
+ // background-color: rgb(222, 223, 220);
+  flex-direction: column;
+  -moz-box-pack: center;
+  justify-content: center;
+  height: 100vh;
+
+
+}
+  .parent{
+    display: grid;
+
+  grid-template-columns: auto 1fr;
+  }
+  .bg-illustration {
+    position: relative;
+    height: 100vh;
+    width: 894px;
+    background: url("${loginbackground}") no-repeat center center scroll;
+    background-size: cover;
+    float: left;
+    -webkit-animation: bgslide 2.3s forwards;
+            animation: bgslide 2.3s forwards;
+  }
+  .bg-illustration img {
+    width: 248px;
+    -webkit-user-select: none;
+       -moz-user-select: none;
+        -ms-user-select: none;
+            user-select: none;
+    height: auto;
+    margin: 19px 0 0 25px;
+  }
+  @media only screen and (min-width: 1024px) and (max-width: 1680px) {
+    .bg-illustration {
+      width: 50vw;
+      -webkit-animation: none;
+              animation: none;
+    }
+  
+    .signinup {
+      width: 50vw;
+    }
+  
+   
+  }
+
+
+  @media only screen and (max-width: 1024px) {
+    body {
+      overflow-x: hidden;
+    }
+  
+    @-webkit-keyframes slideIn {
+      from {
+        left: -100%;
+        opacity: 0;
+      }
+      to {
+        left: 0;
+        opacity: 1;
+      }
+    }
+  
+    @keyframes slideIn {
+      from {
+        left: -100%;
+        opacity: 0;
+      }
+      to {
+        left: 0;
+        opacity: 1;
+      }
+    }
+    .bg-illustration {
+      float: none;
+      background: url("https://i.ibb.co/rwncw7s/bg-mobile.png") center center;
+      background-size: cover;
+      -webkit-animation: slideIn 0.8s ease-in-out forwards;
+              animation: slideIn 0.8s ease-in-out forwards;
+      width: 100%;
+      height: 190px;
+      text-align: center;
+    }
+    .bg-illustration img {
+      width: 100px;
+      height: auto;
+      margin: 20px auto !important;
+      text-align: center;
+    }
+    .bg-illustration .burger-btn {
+      left: 33px;
+      top: 29px;
+      display: block;
+      position: absolute;
+    }
+    .bg-illustration .burger-btn span {
+      display: block;
+      height: 4px;
+      margin: 6px;
+      background-color: #fff;
+    }
+    .bg-illustration .burger-btn span:nth-child(1) {
+      width: 37px;
+    }
+    .bg-illustration .burger-btn span:nth-child(2) {
+      width: 28px;
+    }
+    .bg-illustration .burger-btn span:nth-child(3) {
+      width: 20px;
+    }
+  
+   
+  }
+
+
+  @media only screen and (max-width: 600px)
+  {
+    .bg-illustration {
+     display: none;
     }
   }
 `;
