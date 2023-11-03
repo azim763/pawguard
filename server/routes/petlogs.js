394ec0518ce8acfb597868e6ef5018054856ac54
Cylvia <cylvia.lian@gmail.com>
2023-11-03 12:55:46 -0700

    typography changes;

--- server/routes/petlogs.js
+++ server/routes/petlogs.js
@@ -13,12 +13,8 @@ const {
 // Define routes for Pet Logs
 router.get('/allpetlogs', getAllPetLogs);
 router.post('/create', createPetLog);
-<<<<<<< HEAD
 router.get('/searchpetlogsbypetid/:petID', searchPetLogsByPetID);
 router.get('/getpetlogsbypetid', getPetLogsByPetID);
-=======
-router.get('/searchpetlogsbypetid/', searchPetLogsByPetID);
->>>>>>> 7899d51eb3c20541e224db5809e0337a5c1d941f
 router.get('/getpetlogbyid/:id', getPetLogById);
 router.put('/update/:id', updatePetLogById);
 router.delete('/delete/:id', deletePetLogById);
