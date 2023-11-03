9b250d0a408e57ae5eade3d4606ba5d980a6822a
azim763 <azim763@yahoo.com>
2023-11-03 15:23:53 -0700

    petlog

--- server/controllers/petLogController.js
+++ server/controllers/petLogController.js
@@ -26,8 +26,7 @@ module.exports.createPetLog = async (req, res, next) => {
 // Search for pet logs by PetID
 module.exports.searchPetLogsByPetID = async (req, res, next) => {
     try {
-<<<<<<< HEAD
-     
+
   const petID = req.params.petID;
       const logs = await PetLog.find({ petID: petID });
     //  return req.params;
@@ -42,11 +41,6 @@ module.exports.searchPetLogsByPetID = async (req, res, next) => {
      // return req.params;
       const petID = req.query.PetID;
       const logs = await PetLog.find({ PetID: petID });
-    
-=======
-      const petID = req.query.PetID;
-      const logs = await PetLog.find({ PetID: petID });
->>>>>>> 7899d51eb3c20541e224db5809e0337a5c1d941f
       return res.json(logs);
     } catch (ex) {
       next(ex);
