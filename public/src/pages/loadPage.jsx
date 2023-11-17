f2fa5b6f4b075b6f7ddb2f6ac7650c3eb016f812
Yuki Cheng <hcheng40@mylangara.ca>
2023-11-17 00:01:27 -0800

    [For Review] loading animation

---
+++ public/src/pages/loadPage.jsx
@@ -0,0 +1,10 @@
+import LoadingImage from "../assets/images/loading.gif";
+
+const LoadPage = () => {
+  return (
+    <div>
+      <img src={LoadingImage} />
+    </div>
+  );
+};
+export default LoadPage;
