c739f9e7a1269e5ee181c42048689b13d4b11639
Yuki Cheng <hcheng40@mylangara.ca>
2023-11-25 02:24:17 -0800

    Overall css - abt us img & insurance search

--- public/src/components/AboutUsProfile/AboutUsProfile.jsx
+++ public/src/components/AboutUsProfile/AboutUsProfile.jsx
@@ -1,16 +1,20 @@
-import React from 'react';
+import React from "react";
 import styles from "./AboutUsProfile.module.css";
-import Typography from '../Typography/Typography';
-import LinkedInIcon from "../SVG/LinkedInSVG"
+import Typography from "../Typography/Typography";
+import LinkedInIcon from "../SVG/LinkedInSVG";
 
-const AboutUsProfile = ( { src, alt, title, description, linkedinProfile }) => {
+const AboutUsProfile = ({ src, alt, title, description, linkedinProfile }) => {
   return (
     <div className={styles.AboutUsProfile}>
       <img src={src} alt={alt} />
       <div className={styles.textContainer}>
-        <Typography variant="body2-poppins-medium" color="almost-black">{title}</Typography>
+        <Typography variant="body2-poppins-medium" color="almost-black">
+          {title}
+        </Typography>
         <div className={styles.descriptionContainer}>
-          <Typography variant="body3-poppins-regular" color="almost-black">{description}</Typography>
+          <Typography variant="body3-poppins-regular" color="almost-black">
+            {description}
+          </Typography>
           <a href={linkedinProfile} target="_blank" rel="noopener noreferrer">
             <LinkedInIcon />
           </a>
